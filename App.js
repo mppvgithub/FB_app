09c930664cb01fd8518bfdc568cb6089b777c9e4
mppv <prasanna.uplogic@gmail.com>
2021-03-12 18:52:56 +0530

    home

--- App.js
+++ App.js
@@ -19,7 +19,7 @@ const App = createStackNavigator({
   UDimage:{screen:UDimage}
 },
   {
-    initialRouteName: 'FirstPage',
+    initialRouteName: 'home',
   }
 );
 export default createAppContainer(App);
