5efd2bf4ce82ce9ea7049bb3031eb9b3b48d6488
prasannapv <mppv1000@gmail.com>
2021-07-26 10:51:44 +0530

    IP address

--- config/Constants.js
+++ config/Constants.js
@@ -1,4 +1,5 @@
-// export const BASE_URL = "http://192.168.43.137:9000";
+//prasanna
+export const BASE_URL = "http://192.168.43.137:9000";
 
 // prem
-export const BASE_URL = "http://192.168.43.129:9000";
+// export const BASE_URL = "http://192.168.43.129:9000";
